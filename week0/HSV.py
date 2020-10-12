6385226217a73848329f6f28b16bebaf62facff2
laoSongShu <72282866+laoSongShu@users.noreply.github.com>
2020-10-12 22:50:28 +0800

    HSV tracking code

--- week0/HSV.py
+++ week0/HSV.py
@@ -16,12 +16,10 @@ for index in range(1000):
     lower_blue = np.array([109,50,50])
     upper_blue = np.array([120,255,255])
     
-    #blurred = cv2.GaussianBlur(frame,(11,11),0)
-    #hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     mask = cv2.inRange(hsv,lower_blue,upper_blue)
-    #mask = cv2.inRange(frame,lower_blue,upper_blue)
+    
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
     cnts, hierarchy = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
