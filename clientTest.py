d95932623eb86738e09347a08ecc517997df486d
laoSongShu <72282866+laoSongShu@users.noreply.github.com>
2020-10-22 20:28:49 +0800

    Add files via upload

---
+++ clientTest.py
@@ -0,0 +1,9 @@
+import socket
+client = socket.socket()
+client.connect(('Huaijins-MacBook-Air-4.local',8080))
+#client.send('I am client\n')
+client.send(('I am client\n').encode())
+from_server = client.recv(4096).decode()
+client.close()
+print(from_server)
+
